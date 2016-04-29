678e42c576800cd70127f1a77fc05c19e1217c12
jimmy <jimmylroland@gmail.com>
2016-04-28 21:31:01 -0500

    Initial Commit

---
+++ Roland Grader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roland Grader
+//
+//  Created by Jimmy Roland on 4/28/16.
+//  Copyright © 2016 Jimmy Roland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
